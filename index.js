bb9fe0af8b3edd0672d9ec06ce64e13820e0c365
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-13 19:11:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 //var inst = []
 //var band = []
 function theBeatlesPlay(musicians, instruments){
-  const band = []
   for(let i = 0; i < musicians; i++){
+    const band = []
     band.push(musicians[i] +' plays '+ instruments[i])
   }
   return [band]
