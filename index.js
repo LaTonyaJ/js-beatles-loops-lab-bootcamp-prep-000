a08060fca7797f671b116f7f553c46e8ee591935
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-13 19:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
 //var mus = []
 //var inst = []
-var band = []
+//var band = []
 function theBeatlesPlay(musicians, instruments){
-  //var band = []
+  const band = []
   for(let i = 0; i < musicians; i++){
     band.push(musicians[i] +' plays '+ instruments[i])
   }
